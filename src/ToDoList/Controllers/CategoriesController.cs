f204eaf15d98ed1704f50f6a62687ef0db780a25
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-30 14:51:25 -0700

    export db

---
+++ src/ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            Category thisCategory = db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category thisCategory = db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category thisCategory = db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Category thisCategory = db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            db.Categories.Remove(thisCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
