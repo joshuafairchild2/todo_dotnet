3581e84ed0e1bbb7b4d38522c7d0c3d34871c6a0
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-29 18:45:09 -0700

    complete weekend lesson up to CRUD functionality part 1

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
