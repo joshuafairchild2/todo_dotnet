8b1020ef9e098965a7ee58676c2928df2d1125c5
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-29 19:32:41 -0700

    complete crud functionality part 2

--- src/ToDoList/Controllers/ItemsController.cs
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -14,7 +15,7 @@ namespace ToDoList.Controllers
 
         public IActionResult Details(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
             return View(thisItem);
         }
 
@@ -27,6 +28,35 @@ namespace ToDoList.Controllers
         public IActionResult Create(Item item)
         {
             db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            db.Items.Remove(thisItem);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
